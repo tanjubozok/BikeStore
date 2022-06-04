49bb38e7c147aff24963305c111b68299f658777
Tanju BOZOK <tnjbzk@gmail.com>
2022-06-05 02:47:07 +0400

    Add project files.

---
+++ BikeStore.WinForm/Program.cs
@@ -0,0 +1,20 @@
+﻿using BikeStore.WinForm.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace BikeStore.WinForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
